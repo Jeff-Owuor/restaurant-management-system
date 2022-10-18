623cc5cb1d550c7a0604fd3485e8edcd70c6f27b
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2022-10-18 16:27:59 +0300

    Register user function
    Import AdminController
    Create a route with the AdminController class and give it a name of users.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +16,10 @@ use App\Http\Controllers\HomeController;
 */
 
 Route::get('/',[HomeController::class,'index']);
+Route::get('/users',[AdminController::class,'user']);
 Route::get('/redirects',[HomeController::class,'redirects']);
+
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
