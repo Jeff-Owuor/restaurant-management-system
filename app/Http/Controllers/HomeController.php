d29966846c27b5c80b3384a40472b335a0e201ca
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2022-10-24 13:10:49 +0300

    Display chef's data in home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Food;
+use App\Models\Chefs;
 
 class HomeController extends Controller
 {
     //
     public function index(){
         $data = food::all();
-        return view('home',compact("data"));
+        $chefsdata = chefs::all();
+        return view('home',compact("data","chefsdata"));
     }
     public function redirects(){
         $data = food::all();
