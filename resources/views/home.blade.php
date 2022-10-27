2d62aa536c84b6dbbb6661736355b3bed85422c5
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2022-10-27 13:23:08 +0300

    Provide link to show cart route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -84,15 +84,17 @@ https://templatemo.com/tm-558-klassy-cafe
                             </li>
                             <!-- <li class=""><a rel="sponsored" href="https://templatemo.com" target="_blank">External URL</a></li> -->
                             <li class="scroll-to-section"><a href="#reservation">Contact Us</a></li> 
-                            <li class="scroll-to-section"><a href="#reservation">
+                            <li class="scroll-to-section">
                                 @auth
-                            Cart({{$count}})
-                             @endauth
+                                    <a href="{{url('/showcart',Auth::user()->id)}}">
+                                      Cart({{$count}})
+                                    </a>
+                                @endauth
 
                              @guest
                              Cart(0)
                              @endguest
-                            </a></li> 
+                            </li> 
                             @if (Route::has('login'))
                     @auth
                         <li><x-app-layout></x-app-layout></li>
